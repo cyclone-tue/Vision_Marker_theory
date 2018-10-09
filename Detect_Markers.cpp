7ed3d4a85ed8bf399b3458ac044019d0a9e905ac
Stijn Ringeling <stijnringeling1@gmail.com>
2018-10-09 23:01:56 +0200

    Half of the path planning is implemented in opencv.
    The rest is still Eigen.
    I will try to use an almost full Eigen implementation since it is tested.

--- Detect_Markers.cpp
+++ Detect_Markers.cpp
@@ -4,12 +4,16 @@
 #include <opencv/cv.hpp>
 #include <cmath>
 #include <math.h>
+#include <Eigen/Dense>
+#include <opencv2/core/eigen.hpp>
 
-
-Mat Dimention3(Mat init, Mat beforeHoop, Mat afterHoop, Vec3d hoop_pos);
+using Eigen::MatrixXd;
+using Eigen::Matrix3d;
+using Eigen::VectorXd;
 
 using namespace cv;
 using namespace std;
+using namespace Eigen;
 
 namespace {
     const char* keys =
@@ -42,6 +46,134 @@ static bool readCameraParameters(String filename, OutputArray cameraMatrix, Outp
     //cout << distCoefficients << endl;
     fs.release();
     return true;
+}
+
+MatrixXd equations_f(InputArray M_used, InputArray cond_vec, int j) {
+    Mat M_used_Matrix(6,6, CV_64F);
+    M_used.copyTo(M_used_Matrix);
+    MatrixXd M_u;
+    cv2eigen(M_used_Matrix, M_u);
+
+    Mat cond_vecMatrix(6,1, CV_64F);
+    cond_vec.copyTo(cond_vecMatrix);
+    MatrixXd cond_v;
+    cv2eigen(cond_vecMatrix, cond_v);
+
+    //Map<MatrixXd> cond_v((double *)cond_vecMatrix.data);
+
+    //std::cout << "Here is the matrix A:\n" << M_u << std::endl;
+    //std::cout << "Here is the vector b:\n" << cond_v << std::endl;
+    VectorXd x = M_u.fullPivLu().solve(cond_v);
+    //std::cout << "The solution is:\n" << x << std::endl;
+
+
+    MatrixXd M_full(4, 6);
+    M_full << x(0),x(1),x(2),x(3),x(4),x(5),  0,5*x(0),4*x(1),3*x(2),2*x(3),x(4),  0,0,20*x(0),12*x(1),6*x(2),2*x(3),  0,0,0,60*x(0),24*x(1),6*x(2);
+    return M_full;
+}
+
+void mainm(double iteration, double waypoints, double i, double coef, InputArray cond_finalArray, double t, OutputArray output) {
+
+    Mat cond_final;
+    cond_finalArray.copyTo(cond_final);
+
+    Mat state = Mat::zeros(12,(int) coef, CV_64F);
+
+    Mat cond_vec;
+    cond_final.copyTo(cond_vec);
+    int last = 1;
+    int l1 = 3;
+    int l2 = 3;
+    //Matrix initial data
+    float  data[6*6] = {0,0,0,0,0,1, 0,0,0,0,1,0, 0,0,0,2,0,0, 1,1,1,1,1,1, 5,4,3,2,1,0, 20,12,6,2,0,0};
+    Mat m_used = Mat(6,6, CV_64F, &data);
+
+    //m_used << 0,0,0,0,0,1, 0,0,0,0,1,0, 0,0,0,2,0,0, 1,1,1,1,1,1, 5,4,3,2,1,0, 20,12,6,2,0,0;
+
+    Mat coef_vec_val = Mat::zeros(6,3, CV_64FC4);
+
+    for (int j = 1; j <= 3; j++) {
+        Mat M_full = Mat(4,6, CV_64F);
+        MatrixXd test = equations_f(m_used, cond_vec.col(j-1), j);
+
+        Mat stateField;
+        //statef(coef, M_full, t, stateField);
+        stateField.copyTo(state(Rect(Point((j-1)*4, 0), Size(4,50))));//Copy stateField to ((j-1)*4,0) with size (4,50)
+        Mat coef_vec2 = Mat(6,1, CV_64F);
+    }
+
+    state.copyTo(output);
+}
+
+void Dimention3(InputArray initArray, InputArray p_before_hoop, InputArray finalArray, InputArray hoop_posArray, OutputArray result) {
+
+    Mat init;
+    Mat final;
+    Mat hoop_pos;
+    Mat beforeHoop;
+    initArray.copyTo(init);
+    p_before_hoop.copyTo(beforeHoop);
+    finalArray.copyTo(final);
+    hoop_posArray.copyTo(hoop_pos);
+
+    //the point before the hoop where we still see the hoop
+    Mat p33 = Mat::zeros(3,3,CV_64F);
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 3; j++) {
+            p33.at<double>(i,j) = beforeHoop.at<double>(i,j);
+        }
+    }
+    /*p33(2,0) = 0;
+    p33(2,1) = 0;
+    p33(2,2) = 0;*/
+
+    Mat final_vec = Mat::zeros(6, 6, CV_64F);
+    init.copyTo(final_vec(Rect(Point(0,0), init.size())));//Copy init to 0,0
+    p33.copyTo(final_vec(Rect(Point(0,3), p33.size())));//Copy p33 to 0,3
+    p33.copyTo(final_vec(Rect(Point(3,0), p33.size())));//Copy p33 to 3,0
+    final.copyTo(final_vec(Rect(Point(3,0), final.size())));//Copy final to 3,3
+
+    double coef = 50;
+    int iteration = 0;
+    int t = 0;
+    Mat cond_final = Mat::zeros(6,3, CV_64F);
+    int counter = 0;
+    int t_max = 0;
+    int x = 5;
+    int T_max = 35;
+    int waypoints = 0;
+    double yaw = 0;
+
+    Mat state = Mat::zeros(12, (int)coef, CV_64F);
+    init(Rect(Point(0,0), Size(3,1))).copyTo(state(Rect(Point(0,49), Size(3,1))));//Copy block of size (3,1) from (0,0) from init to state (0,49) of size (3,1)
+    init(Rect(Point(0,1), Size(3,1))).copyTo(state(Rect(Point(4,49), Size(3,1))));//Copy block of size (3,1) from (0,1) from init to state (4,49) of size (3,1)
+    init(Rect(Point(0,2), Size(3,1))).copyTo(state(Rect(Point(8,49), Size(3,1))));//Copy block of size (3,1) from (0,2) from init to state (8,49) of size (3,1)
+
+    Mat trajectory = Mat::zeros(12, (int)((waypoints + 2)*coef), CV_64F);
+
+    for (int i = 1; i <= waypoints + 2; i++) {
+
+        if (iteration >= waypoints) {
+
+            state(Rect(Point(0,49), Size(3,1))).copyTo(cond_final(Rect(Point(0,0), Size(3,1))));
+            state(Rect(Point(4,49), Size(3,1))).copyTo(cond_final(Rect(Point(0,1), Size(3,1))));
+            state(Rect(Point(8,49), Size(3,1))).copyTo(cond_final(Rect(Point(0,2), Size(3,1))));
+            final_vec(Rect(Point(3, (3* (i-1))), Size(3,3))).copyTo(cond_final(Rect(Point(3,0), Size(3,3))));
+
+        }
+        t = 1;
+
+        mainm(iteration, waypoints, i, coef, cond_final,t, state);
+        //        yaw = yaw_math(p_before_hoop(1,1),p_before_hoop(1,2),final(1,1),final(1,2),yaw0,t); need the method
+
+
+        state.copyTo(trajectory(Rect(Point(0, (int)(iteration * 50)), Size(12,50))));
+
+        iteration++;
+    }
+
+
+    trajectory.copyTo(result);
 }
 
 void runPathPlanner(InputArray hoopTransVec, InputArray hoopRotMat, InputOutputArray output, InputArray img){
@@ -80,14 +212,14 @@ void runPathPlanner(InputArray hoopTransVec, InputArray hoopRotMat, InputOutputA
     afterHoop.col(0) = pointFinal;
     afterHoop.col(1) = vel_corr_fin;
 
-    Mat r = Dimention3(init, beforeHoop, afterHoop, hoop_pos);
+    Mat r;
+    MatrixXd test;
+    cv2eigen(init, test);
+    //Dimention3(init, beforeHoop, afterHoop, hoop_pos, r);
     r.copyTo(output);
 
 }
 
-Mat Dimention3(Mat init, Mat beforeHoop, Mat afterHoop, Vec3d hoop_pos) {
-    return Mat();
-}
 
 
 int main(int argc, char* argv[]){
