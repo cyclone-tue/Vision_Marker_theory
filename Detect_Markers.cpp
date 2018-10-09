373918d1e3824f842100203e4a89c4629629ea34
Stijn Ringeling <stijnringeling1@gmail.com>
2018-10-10 00:13:22 +0200

    Fixed pathplanner implementation in Eigen
    TODO: FIx bug related to pathplanner output or visualization.

--- Detect_Markers.cpp
+++ Detect_Markers.cpp
@@ -15,6 +15,8 @@ using namespace cv;
 using namespace std;
 using namespace Eigen;
 
+VectorXd coef_vec2(6);
+
 namespace {
     const char* keys =
             "{cal |    | File to load calibration data from}"
@@ -25,6 +27,7 @@ namespace {
     const double v_after = 1;
     const double d_before = 2;
     const double v_in = 1;
+    const Vec3d hoop_offset = Vec3d(0,0,0);//Offset variable in world space from center of marker to center of hoop.
 
 }
 
@@ -48,95 +51,104 @@ static bool readCameraParameters(String filename, OutputArray cameraMatrix, Outp
     return true;
 }
 
-MatrixXd equations_f(InputArray M_used, InputArray cond_vec, int j) {
-    Mat M_used_Matrix(6,6, CV_64F);
-    M_used.copyTo(M_used_Matrix);
-    MatrixXd M_u;
-    cv2eigen(M_used_Matrix, M_u);
-
-    Mat cond_vecMatrix(6,1, CV_64F);
-    cond_vec.copyTo(cond_vecMatrix);
-    MatrixXd cond_v;
-    cv2eigen(cond_vecMatrix, cond_v);
-
-    //Map<MatrixXd> cond_v((double *)cond_vecMatrix.data);
+MatrixXd equations_f(MatrixXd M_used, VectorXd cond_vec, int j) {
+    MatrixXd M_u(6, 6);
+    M_u = M_used;
+    VectorXd cond_v(6);
+    cond_v = cond_vec;
 
     //std::cout << "Here is the matrix A:\n" << M_u << std::endl;
     //std::cout << "Here is the vector b:\n" << cond_v << std::endl;
     VectorXd x = M_u.fullPivLu().solve(cond_v);
     //std::cout << "The solution is:\n" << x << std::endl;
 
-
+    coef_vec2 = x;
     MatrixXd M_full(4, 6);
     M_full << x(0),x(1),x(2),x(3),x(4),x(5),  0,5*x(0),4*x(1),3*x(2),2*x(3),x(4),  0,0,20*x(0),12*x(1),6*x(2),2*x(3),  0,0,0,60*x(0),24*x(1),6*x(2);
     return M_full;
 }
 
-void mainm(double iteration, double waypoints, double i, double coef, InputArray cond_finalArray, double t, OutputArray output) {
 
-    Mat cond_final;
-    cond_finalArray.copyTo(cond_final);
 
-    Mat state = Mat::zeros(12,(int) coef, CV_64F);
+MatrixXd statef(double coef, MatrixXd M_full, double t) {
 
-    Mat cond_vec;
-    cond_final.copyTo(cond_vec);
+    MatrixXd statef(4, 50);
+
+    MatrixXd M_fullM(4, 6);
+    M_fullM = M_full;
+    //std::cout << M_fullM << std::endl;
+
+    coef = coef / t;
+
+    for (double i = 1.0 / coef; (int)round(i*coef) <= 50; i += (1.0 / coef)) {
+        VectorXd m(6);
+        m << pow(i,5),pow(i,4),pow(i,3),pow(i,2),pow(i,1),1;
+        //std::cout << m << std::endl;
+
+
+        int n = (int) round(i*coef);
+        //std::cout << n << std::endl;
+        MatrixXd result(4, 1);
+        result = M_fullM * m;
+        //std::cout << result << std::endl;
+
+        //populate the nth column with the result of the matrix mult of M_full and t_instant
+        for (int j = 0; j < 4; ++j) {
+            statef(j,n-1) = result(j);
+        }
+    }
+    return statef;
+}
+
+MatrixXd mainm(double iteration, double waypoints, double i, double coef, MatrixXd cond_final, double t) {
+
+    MatrixXd state(12,(int) coef);
+
+    MatrixXd cond_vec = cond_final;
     int last = 1;
     int l1 = 3;
     int l2 = 3;
-    //Matrix initial data
-    float  data[6*6] = {0,0,0,0,0,1, 0,0,0,0,1,0, 0,0,0,2,0,0, 1,1,1,1,1,1, 5,4,3,2,1,0, 20,12,6,2,0,0};
-    Mat m_used = Mat(6,6, CV_64F, &data);
+    MatrixXd m_used(6,6);
+    m_used << 0,0,0,0,0,1, 0,0,0,0,1,0, 0,0,0,2,0,0, 1,1,1,1,1,1, 5,4,3,2,1,0, 20,12,6,2,0,0;
 
-    //m_used << 0,0,0,0,0,1, 0,0,0,0,1,0, 0,0,0,2,0,0, 1,1,1,1,1,1, 5,4,3,2,1,0, 20,12,6,2,0,0;
-
-    Mat coef_vec_val = Mat::zeros(6,3, CV_64FC4);
+    MatrixXd coef_vec_val(6,3);
 
     for (int j = 1; j <= 3; j++) {
-        Mat M_full = Mat(4,6, CV_64F);
-        MatrixXd test = equations_f(m_used, cond_vec.col(j-1), j);
+        MatrixXd M_full(4,6);
+        M_full = equations_f(m_used, cond_vec.col(j-1), j);
 
-        Mat stateField;
-        //statef(coef, M_full, t, stateField);
-        stateField.copyTo(state(Rect(Point((j-1)*4, 0), Size(4,50))));//Copy stateField to ((j-1)*4,0) with size (4,50)
-        Mat coef_vec2 = Mat(6,1, CV_64F);
+        state.block<4,50>((j-1)*4,0) = statef(coef,M_full,t);
+        coef_vec_val.block<6,1>(0,j-1) = coef_vec2;
     }
 
-    state.copyTo(output);
+    return state;
 }
 
-void Dimention3(InputArray initArray, InputArray p_before_hoop, InputArray finalArray, InputArray hoop_posArray, OutputArray result) {
 
-    Mat init;
-    Mat final;
-    Mat hoop_pos;
-    Mat beforeHoop;
-    initArray.copyTo(init);
-    p_before_hoop.copyTo(beforeHoop);
-    finalArray.copyTo(final);
-    hoop_posArray.copyTo(hoop_pos);
+// This method is for path planning
+MatrixXd Dimention3(MatrixXd init, MatrixXd p_before_hoop, MatrixXd final, MatrixXd hoop_pos) {
 
     //the point before the hoop where we still see the hoop
-    Mat p33 = Mat::zeros(3,3,CV_64F);
+    MatrixXd p33(3,3);
     for (int i = 0; i < 2; i++) {
         for (int j = 0; j < 3; j++) {
-            p33.at<double>(i,j) = beforeHoop.at<double>(i,j);
+            p33(i,j) = p_before_hoop(i,j);
         }
     }
-    /*p33(2,0) = 0;
+    p33(2,0) = 0;
     p33(2,1) = 0;
-    p33(2,2) = 0;*/
+    p33(2,2) = 0;
 
-    Mat final_vec = Mat::zeros(6, 6, CV_64F);
-    init.copyTo(final_vec(Rect(Point(0,0), init.size())));//Copy init to 0,0
-    p33.copyTo(final_vec(Rect(Point(0,3), p33.size())));//Copy p33 to 0,3
-    p33.copyTo(final_vec(Rect(Point(3,0), p33.size())));//Copy p33 to 3,0
-    final.copyTo(final_vec(Rect(Point(3,0), final.size())));//Copy final to 3,3
+    MatrixXd final_vec(6, 6);
+    final_vec.block<3, 3>(0, 0) = init;
+    final_vec.block<3, 3>(0, 3) = p33;
+    final_vec.block<3, 3>(3, 0) = p33;
+    final_vec.block<3, 3>(3, 3) = final;
 
     double coef = 50;
     int iteration = 0;
     int t = 0;
-    Mat cond_final = Mat::zeros(6,3, CV_64F);
+    MatrixXd cond_final(6,3);
     int counter = 0;
     int t_max = 0;
     int x = 5;
@@ -144,45 +156,49 @@ void Dimention3(InputArray initArray, InputArray p_before_hoop, InputArray final
     int waypoints = 0;
     double yaw = 0;
 
-    Mat state = Mat::zeros(12, (int)coef, CV_64F);
-    init(Rect(Point(0,0), Size(3,1))).copyTo(state(Rect(Point(0,49), Size(3,1))));//Copy block of size (3,1) from (0,0) from init to state (0,49) of size (3,1)
-    init(Rect(Point(0,1), Size(3,1))).copyTo(state(Rect(Point(4,49), Size(3,1))));//Copy block of size (3,1) from (0,1) from init to state (4,49) of size (3,1)
-    init(Rect(Point(0,2), Size(3,1))).copyTo(state(Rect(Point(8,49), Size(3,1))));//Copy block of size (3,1) from (0,2) from init to state (8,49) of size (3,1)
+    MatrixXd state(12, (int)coef);
+    state.block<3, 1>(0, 49) = init.block<3, 1>(0, 0);
+    state.block<3, 1>(4, 49) = init.block<3, 1>(0, 1);
+    state.block<3, 1>(8, 49) = init.block<3, 1>(0, 2);
 
-    Mat trajectory = Mat::zeros(12, (int)((waypoints + 2)*coef), CV_64F);
+    MatrixXd trajectory(12, (int)((waypoints + 2)*coef));
 
     for (int i = 1; i <= waypoints + 2; i++) {
 
         if (iteration >= waypoints) {
 
-            state(Rect(Point(0,49), Size(3,1))).copyTo(cond_final(Rect(Point(0,0), Size(3,1))));
-            state(Rect(Point(4,49), Size(3,1))).copyTo(cond_final(Rect(Point(0,1), Size(3,1))));
-            state(Rect(Point(8,49), Size(3,1))).copyTo(cond_final(Rect(Point(0,2), Size(3,1))));
-            final_vec(Rect(Point(3, (3* (i-1))), Size(3,3))).copyTo(cond_final(Rect(Point(3,0), Size(3,3))));
+            cond_final.block<3, 1>(0, 0) = state.block<3, 1>(0, 49);
+            cond_final.block<3, 1>(0, 1) = state.block<3, 1>(4, 49);
+            cond_final.block<3, 1>(0, 2) = state.block<3, 1>(8, 49);
+            cond_final.block<3, 3>(3, 0) = final_vec.block<3, 3>(3, (3 * (i - 1)));
 
         }
         t = 1;
 
-        mainm(iteration, waypoints, i, coef, cond_final,t, state);
+        state = mainm(iteration, waypoints, i, coef, cond_final,t);
         //        yaw = yaw_math(p_before_hoop(1,1),p_before_hoop(1,2),final(1,1),final(1,2),yaw0,t); need the method
 
 
-        state.copyTo(trajectory(Rect(Point(0, (int)(iteration * 50)), Size(12,50))));
+        trajectory.block<12, 50>(0, (int)(iteration * 50)) = state;
 
         iteration++;
     }
 
 
-    trajectory.copyTo(result);
+
+    return trajectory;
 }
 
-void runPathPlanner(InputArray hoopTransVec, InputArray hoopRotMat, InputOutputArray output, InputArray img){
+
+
+void runPathPlanner(InputArray hoopTransVec, InputArray hoopRotMat, OutputArray output){
     Mat init = Mat::zeros(3, 1, CV_64FC1);
     Mat R = Mat::zeros(3, 3, CV_64FC1);
     Mat dist_corr_in = Mat::zeros(3, 1, CV_64FC1);
     Mat vel_corr_in = Mat::zeros(3, 1, CV_64FC1);
     Mat dist_corr_fin = Mat::zeros(3, 1, CV_64FC1);
     Mat vel_corr_fin = Mat::zeros(3, 1, CV_64FC1);
+    hoopRotMat.copyTo(R);
 
     Mat distanceBeforeHoop = Mat::zeros(3, 1, CV_64FC1);
     Mat velocityBeforeHoop = Mat::zeros(3, 1, CV_64FC1);
@@ -201,21 +217,28 @@ void runPathPlanner(InputArray hoopTransVec, InputArray hoopRotMat, InputOutputA
     vel_corr_in = R * velocityBeforeHoop;
     vel_corr_fin = R * velocityAfterHoop;
 
-    Mat pointBeforeHoop = hoop_pos - dist_corr_in;
+    Mat pointBeforeHoop = hoop_pos + dist_corr_in;
     Mat beforeHoop = Mat::zeros(2, 3, CV_64F);
     beforeHoop.col(0) = pointBeforeHoop;
     beforeHoop.col(1) = vel_corr_in;
 
     Mat pointFinal = Mat::zeros(1,3, CV_64F);
-    pointFinal = hoop_pos + dist_corr_fin;
+    pointFinal = hoop_pos - dist_corr_fin;
     Mat afterHoop = Mat::zeros(3, 3, CV_64F);
     afterHoop.col(0) = pointFinal;
     afterHoop.col(1) = vel_corr_fin;
 
+    //cout << "Initialized pathplanner variables" << endl;
+
     Mat r;
-    MatrixXd test;
-    cv2eigen(init, test);
-    //Dimention3(init, beforeHoop, afterHoop, hoop_pos, r);
+    MatrixXd initEigen, beforeHoopEigen, afterHoopEigen, hoop_posEigen;
+    cv2eigen(init, initEigen);
+    cv2eigen(beforeHoop, beforeHoopEigen);
+    cv2eigen(afterHoop, afterHoopEigen);
+    cv2eigen(hoop_pos, hoop_posEigen);
+    MatrixXd result = Dimention3(initEigen, beforeHoopEigen, afterHoopEigen, hoop_posEigen);
+    //cout << "Pathplanner executed succesfully" << endl;
+    eigen2cv(result, r);
     r.copyTo(output);
 
 }
@@ -263,17 +286,18 @@ int main(int argc, char* argv[]){
             aruco::estimatePoseSingleMarkers(corners, markerSize, cameraMatrix, distCoef, rvecs, tvecs);
             for(int i=0; i<ids.size(); i++){
                 if(ids[i] == markerId){
-                    Vec3d offset = Vec3d(0,-0.37,0);
-
                     Mat rotMat;
 
-                    Rodrigues(rvecs[i], rotMat);
+                    Rodrigues(rvecs[i], rotMat);//Calculate rotation matrix for marker
 
-                    Mat camOffset = rotMat*Mat(offset);
-                    Vec3d pixelOffset;
+                    Mat pos = rotMat.t()*Mat(tvecs[i]); //Calculate marker position in world space
+                    pos = pos + Mat(hoop_offset); //Add offset in world space to get center of hoop.
+                    //cout << pos <<endl;
 
-                    camOffset.col(0).copyTo(pixelOffset);
-                    tvecs[i] += pixelOffset;
+                    Mat pixelsTranslated = rotMat * pos;
+                    Vec3d pixels;
+                    pixelsTranslated.copyTo(pixels);
+                    tvecs[i] = pixels;
                     double sy = sqrt(pow(rotMat.at<double>(0,0),2) + pow(rotMat.at<double>(1,0), 2));
                     bool singular = sy < 1e-6;
                     double rollTemp, pitchTemp, yawTemp;
@@ -294,20 +318,30 @@ int main(int argc, char* argv[]){
                         pitch -= 2*M_PI;
                     }
 
-                    double x = tvecs[i][0];
-                    double y = tvecs[i][1];
-                    double z = tvecs[i][2];
+                    double x = pos.at<double>(0,0);
+                    double y = pos.at<double>(1,0);
+                    double z = pos.at<double>(2,0);
 
 
 
-                    //double dist = rotMatInversed.dot(tvecs[0] * -1);
-                    //cout << "Roll: " << rollTemp << endl;
-                    //cout << "Pitch: " << pitchTemp << endl;
-                    //cout << "Yaw: "  << yawTemp << endl;
-                    cout << "x " << x << ", y : " << y << ", z :" << z <<  ", yaw: " << yaw/M_PI*180 << ", pitch: " << pitch/M_PI*180 << ", roll: " << roll/M_PI*180 << endl;
+                    //cout << "x " << x << ", y : " << y << ", z :" << z <<  ", yaw: " << yaw/M_PI*180 << ", pitch: " << pitch/M_PI*180 << ", roll: " << roll/M_PI*180 << endl;
                     aruco::drawAxis(imageCopy, cameraMatrix, distCoef, rvecs[i],tvecs[i], markerSize);
                     Mat path;
-                    runPathPlanner(tvecs[i], rotMat, path, imageCopy);
+                    runPathPlanner(pos, rotMat, path);
+
+                    vector<Point3d> pathPoints;
+                    for(int i = 0; i < path.rows; i+=10){
+                        Mat row = path.row(i);
+                        pathPoints.push_back(Point3d(row.at<double>(0,0), row.at<double>(1,0), row.at<double>(2,0)));
+                    }
+                    //cout << "Created path points." << endl;
+                    vector<Point2d> imagePoints;
+                    projectPoints(pathPoints, Vec3d(0,0,0), Vec3d(0,0,0), cameraMatrix, distCoef, imagePoints);
+                    for (int j = 0; j < imagePoints.size() - 1; j++) {
+                        line(imageCopy, imagePoints[j], imagePoints[j + 1], Scalar(255/imagePoints.size() * j,0,255/imagePoints.size()*(imagePoints.size() - j)), 3);
+                    }
+                    //cout << "Drawn lines" << endl;
+                    //cout << path << endl;
                 }
 
             }
